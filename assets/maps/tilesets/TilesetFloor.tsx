d0338eea4c3e5e41e053ba03f40582c97ae5b927
Oleksii Stadnyk <doctorstal@gmail.com>
2025-03-10 16:51:04 +0100

    Added map render

--- assets/maps/tilesets/TilesetFloor.tsx
+++ assets/maps/tilesets/TilesetFloor.tsx
@@ -1,4 +1,33 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TilesetFloor" tilewidth="16" tileheight="16" tilecount="572" columns="22">
  <image source="../../../../assets/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetFloor.png" width="352" height="417"/>
+ <wangsets>
+  <wangset name="grass-corner" type="corner" tile="154">
+   <wangcolor name="dirt" color="#ff0000" tile="177" probability="1"/>
+   <wangcolor name="grass" color="#00ff00" tile="267" probability="1"/>
+   <wangtile tileid="154" wangid="2,2,0,1,2,2,2,2"/>
+   <wangtile tileid="155" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="156" wangid="0,2,2,2,0,1,0,2"/>
+   <wangtile tileid="163" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="176" wangid="0,1,0,1,0,2,2,2"/>
+   <wangtile tileid="177" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="178" wangid="0,2,2,2,0,1,0,1"/>
+   <wangtile tileid="181" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="182" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="185" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="198" wangid="0,1,0,2,2,2,2,2"/>
+   <wangtile tileid="199" wangid="0,1,0,2,2,2,0,1"/>
+   <wangtile tileid="200" wangid="0,2,2,2,2,2,0,1"/>
+   <wangtile tileid="203" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="204" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="229" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="244" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="245" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="264" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="265" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="266" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="267" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="268" wangid="0,2,0,2,0,2,0,2"/>
+  </wangset>
+ </wangsets>
 </tileset>
--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"image"
 	"image/color"
 	"log"
+	"math"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+const TileWidth = 16
 
 type Sprite struct {
 	Img       *ebiten.Image
@@ -34,52 +38,50 @@ type Potion struct {
 }
 
 type Game struct {
-	player  *Player
-	enemies []*Enemy
-	potions []*Potion
+	player      *Player
+	enemies     []*Enemy
+	potions     []*Potion
+	tilemapJSON *TilemapJSON
+	tilemapImg  *ebiten.Image
 }
 
 func (g *Game) Update() error {
 	// react to key presses
 
-	g.player.Frame += 1
+	playerMoves := true
 	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
-		g.player.X += 2
 		g.player.Direction = 3
 	} else if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
-		g.player.X -= 2
 		g.player.Direction = 2
 	} else if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
-		g.player.Y -= 2
 		g.player.Direction = 1
 	} else if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
-		g.player.Y += 2
 		g.player.Direction = 0
 	} else {
-		g.player.Frame -= 1
+		playerMoves = false
 	}
-	g.player.Frame %= 256
+	if playerMoves {
+		g.player.Move(2)
+	}
 
 	for _, enemy := range g.enemies {
-		if enemy.FollowsPlayer {
-			enemy.Frame += 1
+		if enemy.FollowsPlayer && enemy.Dist(g.player.Sprite) < 5*TileWidth {
 			if enemy.X < g.player.X {
-				enemy.X += 1
 				enemy.Direction = 3
 			} else if enemy.X > g.player.X {
-				enemy.X -= 1
 				enemy.Direction = 2
 			} else if enemy.Y > g.player.Y {
-				enemy.Y -= 1
 				enemy.Direction = 1
 			} else if enemy.Y < g.player.Y {
-				enemy.Y += 1
 				enemy.Direction = 0
-			} else {
-				enemy.Frame -= 1
 			}
-			enemy.Frame %= 256
+
+		} else {
+			if rand.Float64() > 0.95 {
+				enemy.Direction = rand.Intn(4)
+			}
 		}
+		enemy.Move(1)
 	}
 
 	for _, potion := range g.potions {
@@ -94,6 +96,26 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{0x33, 0x66, 0x99, 255})
+
+	opts := &ebiten.DrawImageOptions{}
+	for _, layer := range g.tilemapJSON.Layers {
+		for index, id := range layer.Data {
+			x := TileWidth * (index % layer.Width)
+			y := TileWidth * (index / layer.Width)
+
+			srcX := TileWidth * ((id - 1) % 22)
+			srcY := TileWidth * ((id - 1) / 22)
+
+			opts.GeoM.Translate(float64(x), float64(y))
+
+			screen.DrawImage(
+				g.tilemapImg.SubImage(image.Rect(srcX, srcY, srcX+TileWidth, srcY+TileWidth)).(*ebiten.Image),
+				opts,
+			)
+			opts.GeoM.Reset()
+		}
+	}
+
 	// draw player
 	DrawSprite(g.player.Sprite, screen)
 
@@ -110,16 +132,34 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("Player Health: %d \n", g.player.Health))
 }
 
+func (s *Sprite) Dist(other *Sprite) float64 {
+	return math.Sqrt(math.Pow(s.X-other.X, 2) + math.Pow(s.Y-other.Y, 2))
+}
+
+func (s *Sprite) Move(d float64) {
+	switch s.Direction {
+	case 0:
+		s.Y += d
+	case 1:
+		s.Y -= d
+	case 2:
+		s.X -= d
+	case 3:
+		s.X += d
+	}
+	s.Frame = (s.Frame + 1) % 256
+}
+
 func DrawSprite(sprite *Sprite, screen *ebiten.Image) {
 	opts := &ebiten.DrawImageOptions{}
 	opts.GeoM.Translate(sprite.X, sprite.Y)
 
-	dx := 16 * sprite.Direction
-	fy := 16 * (sprite.Frame / 4 % 4)
+	dx := TileWidth * sprite.Direction
+	fy := TileWidth * (sprite.Frame / 4 % 4)
 
 	screen.DrawImage(
 		sprite.Img.SubImage(
-			image.Rect(dx, fy, 16+dx, 16+fy),
+			image.Rect(dx, fy, TileWidth+dx, TileWidth+fy),
 		).(*ebiten.Image),
 		opts,
 	)
@@ -146,6 +186,16 @@ func main() {
 	}
 
 	potionImg, _, err := ebitenutil.NewImageFromFile("assets/images/LifePot.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tilemapImg, _, err := ebitenutil.NewImageFromFile("assets/images/TilesetFloor.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tilemapJSON, err := NewTilemapJSON("assets/maps/first.tmj")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -196,7 +246,10 @@ func main() {
 				AmtHeal: 1,
 			},
 		},
+		tilemapJSON: tilemapJSON,
+		tilemapImg:  tilemapImg,
 	}
+
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
